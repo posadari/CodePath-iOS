36c91c5f57dc9dfa16376d212783200b6ab406b2
posadari <57573681+posadari@users.noreply.github.com>
2021-12-29 22:44:46 -0600

    implement prework features

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateTip(_ sender: Any) {
+        // get bill amount from input
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        // get tip by tip * tipPercentage
+        let tipPercentages = [0.15, 0.8, 0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // update tip amount label
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        // update total amount
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
 }
 
